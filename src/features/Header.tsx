55ebb63184a8aedcbc0a61ee5974e9bde1850886
kharle14 <enriquezkharl14@gmail.com>
2024-01-30 11:24:12 +0800

    feature: Header component
    - Added React Icons

---
+++ src/features/Header.tsx
@@ -0,0 +1,26 @@
+import { Navbar, NavbarBrand } from '@nextui-org/react';
+import { FaCloud } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <Navbar isBordered>
+      <NavbarBrand>
+        <FaCloud size={48} />
+        <p className="ml-2 text-lg font-bold">Weather Forecast</p>
+      </NavbarBrand>
+
+      {/* <NavbarContent justify="end">
+        <NavbarItem>
+          <Button
+            as={Link}
+            color="primary"
+            href="#"
+            variant="flat"
+          >
+            Log Out
+          </Button>
+        </NavbarItem>
+      </NavbarContent> */}
+    </Navbar>
+  );
+}
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,6 +3,13 @@
 import { NextUIProvider } from '@nextui-org/react';
 import type { PropsWithChildren } from 'react';
 
+import Header from '@/features/Header';
+
 export default function Providers({ children }: PropsWithChildren) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <Header />
+      {children}
+    </NextUIProvider>
+  );
 }
