a73e8a6d41e4022cb5477ce3286812381b7670fa
kharle14 <enriquezkharl14@gmail.com>
2024-01-30 11:51:34 +0800

    Next Auth
    - Added Next Auth
    - Github config

---
+++ src/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth/next';
+import Github from 'next-auth/providers/github';
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
+  throw new Error('Missing github oauth credentials!');
+
+export const {
+  handler: { GET, POST },
+  auth,
+  signIn,
+  SignOut,
+} = NextAuth({
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
